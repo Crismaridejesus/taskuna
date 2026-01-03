143f36efff3125fef6cec2dc59496892181b614b
Maricris <maricris052298dejesus@gmail.com>
2026-01-03 13:01:01 +0800

    feat: add DashboardPage and PagesLayout components with sidebar and header

---
+++ app/(pages)/dashboard/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+import MainContainer from "@/components/dashboard/components/MainContainer/MainContainer";
+
+function DashboardPage() {
+    return (
+        <>
+            <MainContainer />
+        </>
+    );
+}
+
+export default DashboardPage;
---
+++ app/(pages)/layout.tsx
@@ -0,0 +1,28 @@
+import Header from "@/components/shared/header/Header";
+import SidebarLeft from "@/components/shared/sidebar/sidebarLeft/SideBarLeft";
+import SidebarRight from "@/components/shared/sidebar/sidebarRight/SideBarRight";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { cookies } from "next/headers";
+
+export default async function PagesLayout({ children }: { children: React.ReactNode }) {
+    const cookieStore = await cookies();
+    const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
+
+    return (
+        <SidebarProvider defaultOpen={defaultOpen}>
+            <div className="grid min-h-screen w-full grid-cols-[auto_1fr_400px] grid-rows-[70px_1fr]">
+                <header className="col-span-2 col-start-2 row-span-1">
+                    <Header />
+                </header>
+                <aside className="col-span-1 row-span-2 row-start-1">
+                    <SidebarLeft />
+                </aside>
+                <main className="col-span-1 row-span-1 row-start-2 h-full w-full">{children}</main>
+
+                <aside className="col-span-1 col-start-3 row-span-1 row-start-2 bg-blue-300">
+                    <SidebarRight />
+                </aside>
+            </div>
+        </SidebarProvider>
+    );
+}
